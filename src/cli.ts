d609eb6d521f81ea963f1e790792123466c14906
michiharu <michi1diver@gmail.com>
2023-03-26 00:22:38 +0900

    :+1: yargsを導入して大幅リファクタリング

---
+++ src/cli.ts
@@ -0,0 +1,116 @@
+#!/usr/bin/env node
+
+import yargs from 'yargs/yargs';
+
+import scan from './index.js';
+
+const filesCount = (count: number) =>
+  count === 1 ? ('1 file' as const) : (`${count} files` as const);
+
+const red = (text: string) => `\x1b[31m${text}\x1b[0m`;
+const green = (text: string) => `\x1b[32m${text}\x1b[0m`;
+const bgRed = (text: string) => `\x1b[41m${text}\x1b[49m`;
+const bgGreen = (text: string) => `\x1b[42m${text}\x1b[49m`;
+
+function run() {
+  const argv = yargs(process.argv.slice(2))
+    .usage(
+      `
+✨Welcome to Scandog🐕
+Scandog checks if there exist files with the specified suffix for all files matching globs.
+
+Please visit https://github.com/sindresorhus/globby#readme for how to write glob. Scandog uses "globby" as glob engine.
+
+Usage: scandog <suffix> <patterns...> <options>
+
+Example: scandog .spec '**/*.ts' --gitignore`
+    )
+    .command('* <suffix> <patterns...>', false)
+    .positional('suffix', {
+      describe: 'suffix',
+      type: 'string',
+      demandOption: true,
+    })
+    .positional('patterns', {
+      describe: `glob patterns.`,
+      type: 'string',
+      array: true,
+      default: ['**/*.{js,jsx,ts,tsx}'],
+    })
+    .options({
+      gitignore: {
+        type: 'boolean',
+        describe: 'Apply ignore patterns in ".gitignore" files.',
+        default: false,
+      },
+    })
+    .alias({
+      h: 'help',
+      v: 'version',
+      g: 'gitignore',
+    })
+    .demandCommand(1)
+    .parseSync();
+
+  const { suffix, patterns, gitignore } = argv;
+  console.log();
+  console.log(`   suffix: ${suffix}`);
+  if (patterns.length === 1) {
+    console.log(`  pattern: ${patterns[0]}`);
+  } else {
+    console.log(' patterns:');
+    patterns.forEach((p) => console.log(`  - ${p}`));
+  }
+  console.log(`gitignore: ${gitignore}`);
+  console.log();
+
+  patterns.push('!node_modules');
+  const results = scan(suffix, patterns, { gitignore });
+
+  const errors = results.filter(({ exists }) => !exists);
+
+  if (errors.length === 1) {
+    const { path, withSuffix } = errors[0];
+    console.error(`Does not exist: ./${withSuffix}`);
+    console.error(`               (./${path})`);
+  }
+
+  if (errors.length > 1) {
+    console.error('List of files that do not exist:');
+    errors.forEach(({ path, withSuffix }) => {
+      console.error(`  - ./${withSuffix}`);
+      console.error(`   (./${path})`);
+    });
+  }
+
+  // summary
+  if (results.length === 0) {
+    console.error('\x1b[31mNo matching files.\x1b[0m');
+    process.exitCode = 2;
+    return;
+  }
+
+  if (errors.length !== 0) console.log();
+
+  console.log(`Found ${filesCount(results.length)}.`);
+  if (errors.length !== 0) {
+    const message =
+      errors.length === 1 ? '1 file does not exist.' : `${errors.length} do not exist.`;
+    console.log(red(message));
+  }
+  const existCount = results.length - errors.length;
+  if (existCount !== 0) {
+    const message = existCount === 1 ? '1 file exists.' : `${existCount} files exist.`;
+    console.log(message);
+  }
+
+  if (errors.length === 0) {
+    const message = `${bgGreen('SUCCESS')} All files with "${suffix}" exist.`;
+    console.log();
+  } else {
+    process.exitCode = 1;
+  }
+  console.log();
+}
+
+run();
--- src/index.ts
+++ src/index.ts
@@ -1,71 +1,35 @@
-#!/usr/bin/env node
-
-import { globbySync } from 'globby';
 import fs from 'node:fs';
 import { parse } from 'node:path';
-import Reporter from './reporter.js';
 
-function getWithSuffix(path: string, suffix: string) {
+import { globbySync } from 'globby';
+
+type ScanOptions = {
+  gitignore: boolean;
+};
+
+type PathResult = {
+  path: string;
+  withSuffix: string;
+  exists: boolean;
+};
+
+export const isTargetPath = (suffix: string) => (path: string) => {
+  const { name } = parse(path);
+  return !name.endsWith(suffix);
+};
+
+export function getWithSuffix(path: string, suffix: string) {
   const { dir, name, ext } = parse(path);
   if (dir === '') return `${name}${suffix}${ext}`;
   return `${dir}/${name}${suffix}${ext}`;
 }
 
-const isTarget = (suffix: string) => (path: string) => {
-  if (path.includes('node_modules')) return false;
-  const { name } = parse(path);
-  return !name.includes(suffix);
+const scan = (suffix: string, patterns: string[], { gitignore }: ScanOptions): PathResult[] => {
+  const paths = globbySync(patterns, { gitignore }).filter(isTargetPath(suffix));
+  return paths.map((path) => {
+    const withSuffix = getWithSuffix(path, suffix);
+    return { path, withSuffix, exists: fs.existsSync(withSuffix) };
+  });
 };
 
-const check = (suffix: string, reporter: Reporter) => (path: string) => {
-  const withSuffix = getWithSuffix(path, suffix);
-  const exists = fs.existsSync(withSuffix);
-  if (exists) {
-    reporter.reportExist();
-  } else {
-    reporter.reportNotExist(withSuffix, path);
-  }
-};
-
-function scan(patterns: string[], suffix: string) {
-  const paths = globbySync(patterns).filter(isTarget(suffix));
-  const reporter = new Reporter(paths);
-  paths.forEach(check(suffix, reporter));
-  reporter.printSummary();
-}
-
-function showVersion() {
-  console.log(JSON.stringify(process.env, undefined, 2));
-  const json = fs.readFileSync(new URL('package.json', import.meta.url), 'utf8');
-  const { name, version } = JSON.parse(json);
-  console.log(`${name} ${version}`);
-}
-
-function showHelpInformation() {
-  console.log(`
-Usage: scandog [suffix] [file/glob ...]
-   ex. scandog .spec src/**/*.ts
-
-Verify that all files matching a given suffix have been correctly identified by the provided glob pattern.
-The default glob is "**/*.{js,jsx,ts,tsx}".
-Given a suffix of "spec" and a glob expression of "**/*.ts",
-check that for each ".ts" file matching the glob expression, there exists a corresponding ".spec.ts" file.
-
-  -h, --help     Display this help
-  -v, --version  Display the package version
-`);
-}
-
-function run() {
-  const args = process.argv.slice(2);
-
-  if (args.some((a) => ['--help', '-h'].includes(a))) return showHelpInformation();
-  if (args.some((a) => ['--version', '-v'].includes(a))) return showVersion();
-
-  const suffix = args[0];
-  const patterns = args.slice(1);
-  if (patterns.length === 0) patterns.push('**/*.{js,jsx,ts,tsx}');
-  scan(patterns, suffix);
-}
-
-run();
+export default scan;
