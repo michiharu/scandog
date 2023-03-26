a8db65c420fc9fc4eec7e8a741a400e54350539a
michiharu <michi1diver@gmail.com>
2023-03-27 07:45:19 +0900

    :green_heart: vitestの導入

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,1 +1,9 @@
-export {};
+import { describe, expect, test } from 'vitest';
+
+import { isTargetPath } from './index';
+
+describe('isTargetPath', () => {
+  test('directory/name.ts', () => expect(isTargetPath('-suffix')('directory/name.ts')).toBe(true));
+  test('directory/name-suffix.ts', () =>
+    expect(isTargetPath('-suffix')('directory/name-suffix.ts')).toBe(false));
+});
